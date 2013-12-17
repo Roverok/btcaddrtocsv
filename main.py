190949a5c9dcbdf147cbca056136e025e9ef7ef8
chalbersma <chalbersma.12@gmail.com>
2013-12-17 18:22:21 +0000

    Doing some Testing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 # -h Help
 # -a BTCAddress
 # -o CSV Output File
+# -x Existing CSV File
 parser.add_argument("-a", "--btcaddress", help="Bitcoin Address", required=True)
 parser.add_argument("-o", "--outputfile", help="CSV Formatted Output File", default="out.csv")
 parser.add_argument("-x", "--existing", help="CSV Formatted Existing File")
