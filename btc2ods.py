d2059e190e00b4e22f43acda62a0d8d8c8ead015
Chris Halbersma <chalbersma.12@gmail.com>
2014-10-09 12:09:53 -0500

    Bringing in Lxml 3.4.0

--- btc2ods.py
+++ btc2ods.py
@@ -5,37 +5,16 @@
 # Small hack to allow using the odslib library without having the
 # client add it to their system manually
 import sys
-sys.path.insert(0, './odslib')
+sys.path.insert(0, './ezodf')
 
-from odslib import ODS
+from ezodf import newdoc, Sheet
 
 def test():
-    print ("Test")
-    # Create a New Sheet
-    ods = ODS()
-
-    # Creat Sheet Title
-    sheet = ods.content.getSheet(0)
-    sheet.setSheetName('Test')
-
-    # Add some content
-    sheet.getCell(0,0).stringValue("Some Cool Text")
-    sheet.getCell(1,0).floatValue(2)
-
-    # Add some more content
-    sheet.getCell(0,1).stringValue("Some More Cooler Text")
-    sheet.getCell(1,1).floatValue(3)
-
-    # Add a Formula
-    sheet.getCell(0,2).stringValue("Together Now")
-    sheet.getCell(1,2).floatFormula(0, '=SUM(B1:B2)')
-
-    # Create Sheet2
-    ods.content.makeSheet("Second Sheet")
-
-    # Generate Response and Save
-    ods.save("test.ods")
-    return 
+    ods = newdoc(doctype='ods', filname='test.ods')
+    sheet1 = Sheet('sheet1')
+    ods.sheets += sheet1
+    sheet['A1'].set_value("Test")
+    ods.save()
 
 
 test()
